60aa52bfc03778fbc4e0dc24cb1e3541bcb92669
asrealmadrid97 <asrealmadrid97@gmail.com>
2020-01-27 17:40:30 -0500

    updated UI

--- main.go
+++ main.go
@@ -6,11 +6,16 @@ package main
 import (
 	"fmt"
 	"net/http"
+	// "net"
 	"log"
 	"github.com/gorilla/websocket"
 	"sync"
 	"time"
 )
+
+var wg sync.WaitGroup
+var upgrader = websocket.Upgrader{}
+// var upgrader2 = websocket.Upgrader{}
 
 type myStruct struct {
 	Username string `json:"Username"`
@@ -18,11 +23,8 @@ type myStruct struct {
 	LastName string `json:"lastname"`
 }
 
-var wg sync.WaitGroup
-var upgrader = websocket.Upgrader{}
-
 func index1(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "index2.html")
+	http.ServeFile(w, r, "index1.html")
 }
 
 func index2(w http.ResponseWriter, r *http.Request) {
@@ -41,36 +43,27 @@ func ws(w http.ResponseWriter, r *http.Request) {
 	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		// fmt.Fprintf(w, "Error in establishing connection")
 		log.Println(string("Checking...."))
 		log.Fatal(err)
 	}
 
 	go func(conn *websocket.Conn) {
-			ch := time.Tick(5 *time.Second)
-			counter := 0
+			ch := time.Tick(100 *time.Millisecond)
+			// counter := 0
 
 			for range ch {
-				msg := []byte(string(counter))
-				conn.WriteMessage(websocket.TextMessage, msg)
-				counter += 1
-				// conn.WriteJSON(myStruct{
-				// 	Username:"ssr.sameersingh",
-				// 	FirstName:"Sameer Singh",
-				// 	LastName:"Rathor",
-				// })
+				mType, msg, err := conn.ReadMessage()
+				if err != nil {
+					log.Printf("Failed to read message, %v", err)
+				}
+
+				conn.WriteMessage(mType, msg)
+				// msg := []byte(string(counter))
+				// conn.WriteMessage(websocket.TextMessage, msg)
+				// counter += 1
 			}
 		}(conn)
-
-	// defer conn.Close()
-	// msg := []byte("Lets start to talk something.")
-	// err = conn.WriteMessage(websocket.TextMessage, msg)
-	// if err != nil {
-	// 	log.Println(err)
-	// }
-	// wg.Add(1)
 	func(conn *websocket.Conn) { //reader function
-		// defer wg.Done()
 		for {
 			mType, msg, err := conn.ReadMessage()
 			if err != nil {
@@ -78,7 +71,6 @@ func ws(w http.ResponseWriter, r *http.Request) {
 				conn.Close()
 				return
 			}
-			// log.Println(string("yolo"))
 			log.Println(string(msg)) // print incoming message
 			if err := conn.WriteMessage(mType, msg); err != nil { //echo the message back to client
 				log.Println(err)
@@ -86,23 +78,6 @@ func ws(w http.ResponseWriter, r *http.Request) {
 			} 
 		}
 	}(conn)
-	// wg.Wait()
-	// conn.Close()
-
-	// go func(conn *websocket.Conn) {
-	// 	conn.WriteMessage(mtype, msg)
-	// }(conn)
-
-	// for {
-	// 	mType, msg, err := conn.ReadMessage()
-	// 	if err != nil {
-	// 		log.Printf("failed to read message %v", err)
-	// 		conn.Close()
-	// 		return
-	// 	}
-	// 	log.Println(string(msg))
-	// 	conn.WriteMessage(mType, msg)
-	// }
 	fmt.Fprintf(w, "Connection Established sucessfully")
 	fmt.Fprintf(w, "YAY !")
 }
@@ -114,5 +89,7 @@ func main() {
 	http.HandleFunc("/index3", index3)
 	http.HandleFunc("/index4", index4)
 	http.HandleFunc("/ws", ws)
+	// http.HandleFunc("/send", ss)
+
 	http.ListenAndServe(":8123", nil)
 }
--- runny_client.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"fmt"
-	"net/http"
-	"log"
-	gorilla_ws"github.com/gorilla/websocket"
-	golang_ws"golang.org/x/net/websocket"
-)
