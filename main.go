be4f02a5ffdb155d81ee6f70cbdfa63e9e8be37b
asrealmadrid97 <asrealmadrid97@gmail.com>
2020-01-28 11:02:22 -0500

    chat boradcast, updated assets folder added htmls

--- main.go
+++ main.go
@@ -6,63 +6,77 @@ package main
 import (
 	"fmt"
 	"net/http"
-	// "net"
 	"log"
 	"github.com/gorilla/websocket"
 	"sync"
-	"time"
+	// "time"
 )
 
 var wg sync.WaitGroup
 var upgrader = websocket.Upgrader{}
 // var upgrader2 = websocket.Upgrader{}
 
-type myStruct struct {
-	Username string `json:"Username"`
-	FirstName string `json:"firstName"`
-	LastName string `json:"lastname"`
+type longLatStruct struct {
+	Long float64 `json:"longitute"`
+	Lat float64 `jason:"latitude"`
 }
 
+var clients = make(map[*websocket.Conn]bool)
+var broadcast = make(chan *longLatStruct)
+
 func index1(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "index1.html")
+	log.Println("index1 fun")
+	http.ServeFile(w, r, "assets/index1.html")
 }
 
 func index2(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "index2.html")
+	log.Println("index2 fun")
+	http.ServeFile(w, r, "assets/index2.html")
 }
 
 func index3(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "index3.html")
+	log.Println("index3 fun")
+	http.ServeFile(w, r, "assets/index3.html")
 }
 
 func index4(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "index4.html")
+	log.Println("index4 fun")
+	http.ServeFile(w, r, "assets/index4.html")
 }
 
 func ws(w http.ResponseWriter, r *http.Request) {
 	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
 	conn, err := upgrader.Upgrade(w, r, nil)
+	log.Println("connection upgraded fun")
+	clients[conn] = true
 	if err != nil {
 		log.Println(string("Checking...."))
 		log.Fatal(err)
 	}
 
-	go func(conn *websocket.Conn) {
-			ch := time.Tick(100 *time.Millisecond)
-			// counter := 0
+	// go func(conn *websocket.Conn) {
+	// 		ch := time.Tick(100 *time.Millisecond)
+	// 		// counter := 0
 
-			for range ch {
-				mType, msg, err := conn.ReadMessage()
-				if err != nil {
-					log.Printf("Failed to read message, %v", err)
-				}
-
-				conn.WriteMessage(mType, msg)
-				// msg := []byte(string(counter))
-				// conn.WriteMessage(websocket.TextMessage, msg)
-				// counter += 1
-			}
-		}(conn)
+	// 		for range ch {
+	// 			mType, msg, err := conn.ReadMessage()
+	// 			if err != nil {
+	// 				log.Printf("Failed to read message, %v", err)
+	// 			}
+	// 			for client := range clients {
+	// 				err := client.WriteMessage(websocket.TextMessage, msg)
+	// 				if err != nil {
+	// 					log.Printf("Error sending message: %v", err)
+	// 					client.Close()
+	// 					delete(clients, client)
+	// 				}
+	// 			}
+	// 			conn.WriteMessage(mType, msg)
+	// 			// msg := []byte(string(counter))
+	// 			// conn.WriteMessage(websocket.TextMessage, msg)
+	// 			// counter += 1
+	// 		}
+	// 	}(conn)
 	func(conn *websocket.Conn) { //reader function
 		for {
 			mType, msg, err := conn.ReadMessage()
@@ -71,11 +85,19 @@ func ws(w http.ResponseWriter, r *http.Request) {
 				conn.Close()
 				return
 			}
+			for client := range clients {
+					err := client.WriteMessage(mType, msg)
+					if err != nil {
+						log.Printf("Error sending message: %v", err)
+						client.Close()
+						delete(clients, client)
+					}
+				}
 			log.Println(string(msg)) // print incoming message
-			if err := conn.WriteMessage(mType, msg); err != nil { //echo the message back to client
-				log.Println(err)
-				return
-			} 
+			// if err := conn.WriteMessage(mType, msg); err != nil { //echo the message back to client
+			// 	log.Println(err)
+			// 	return
+			// } 
 		}
 	}(conn)
 	fmt.Fprintf(w, "Connection Established sucessfully")
